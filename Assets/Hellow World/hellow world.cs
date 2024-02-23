7ab2130b3c25f7497ad2217bb35ca01f29d3ab9b
AlexF093 <alexander.f@opendeusto.es>
2024-02-23 08:55:22 +0100

    Nuevo codigo

---
+++ Assets/Hellow World/hellow world.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace;
+
+public class hellowworld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy enemy = new Enemy("Voldemor", 100, true);
+        Player player = new Player("HP", 500, true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
